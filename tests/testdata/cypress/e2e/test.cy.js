a8c7142c20cb587a57037885af898526fff9f231
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-12-16 14:02:33 +0900

    add Cypress tests

---
+++ tests/testdata/cypress/e2e/test.cy.js
@@ -0,0 +1,33 @@
+describe('Cypress-cake', () => {
+  it('manages the database', () => {
+    cy.clearDatabase().then((response) => {
+      expect(response.data).to.equal(true)
+    })
+    cy.visit('/users')
+    cy.get('table').should('not.contain', 'now@example.com')
+
+    /**
+     * This test will fail if the file does not exist. Because we are running in a browser environment, we don't have
+     * access to the filesystem to dynamically create the file. We create it in the BATS test.
+     */
+    cy.restoreDatabase('/tmp/test.sql')
+
+    cy.visit('/users')
+    cy.get('table').should('contain', 'now@example.com')
+
+    cy.clearDatabase()
+    cy.visit('/users')
+    cy.get('table').should('not.contain', 'now@example.com')
+  })
+
+  it('creates a user', () => {
+    const email = 'example@example.com'
+    cy.create('User', { email }).then((response) => {
+      expect(response.status).to.equal(200)
+      expect(response.body.data.email).to.equal(email)
+    })
+
+    cy.visit('/users')
+    cy.get('table').should('contain', email)
+  })
+})
---
+++ tests/testdata/src/Controller/UsersController.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * Users Controller
+ *
+ * @property \App\Model\Table\UsersTable $Users
+ */
+class UsersController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        $query = $this->Users->find();
+        $users = $this->paginate($query);
+
+        $this->set(compact('users'));
+    }
+}
